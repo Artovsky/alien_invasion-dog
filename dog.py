93ccd9a83df6f64c5ab933c87f4c9d6c920ea0a1
Artovsky <arlok2211@gmail.com>
2021-04-25 21:02:06 +0300

    Add files via upload

---
+++ dog.py
@@ -0,0 +1,21 @@
+import pygame as pg
+import sys
+
+W = 600
+H = 600
+
+sc = pg.display.set_mode ( (W, H) )
+sc.fill ( (100, 150, 200) )
+
+dog_surf = pg.image.load ( 'dog.bmp' )
+dog_rect = dog_surf.get_rect ( bottomright=(W, H) )
+sc.blit ( dog_surf, dog_rect )
+
+pg.display.update ()
+
+while 1:
+    for i in pg.event.get ():
+        if i.type == pg.QUIT:
+            sys.exit ()
+
+    pg.time.delay ( 20 )
